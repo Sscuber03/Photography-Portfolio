fa01c3f76e8c09df7c830c5f1153572ca0ad4343
Sscuber03 <sambit2003sarkar@gmail.com>
2024-07-24 18:50:37 +0530

    set up of pages

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+//import icons
+import {
+  ImFacebook,
+  ImTwitter,
+  ImInstagram,
+} from 'react-icons/im';
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return <div className='hidden xl:flex ml-24'>
+    <ul className='flex gap-x-4'>
+      <li><a href='https://www.instagram.com/ss_cuber2/' target='_blank' rel='noreferrer'><ImInstagram /></a></li>
+      <li><a href='https://www.facebook.com/sambit.sarkar2003/' target='_blank' rel='noreferrer'><ImFacebook /></a></li>
+      <li><a href='https://x.com/Sambits28225539' target='_blank' rel='noreferrer'><ImTwitter /></a></li>
+    </ul>
+  </div>;
 };
 
 export default Socials;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className='section bg-orange-200'>
+      Contact
+    </section>
+  );
 };
 
 export default Contact;
